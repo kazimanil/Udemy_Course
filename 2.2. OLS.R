44f16dba73de7fce8fade9a9aa45dd17f0cae7ed
kazimanil <kazimanileren@gmail.com>
2017-12-08 18:37:37 +0300

    My codes for Lecture 2.2 or Section 10

---
+++ 2.2. OLS.R
@@ -0,0 +1,10 @@
+rm(list = ls()); gc(); dev.off()
+setwd(dir = "Udemy/2. Modelling/")
+sd <- fread(input = "2.2. SalaryData.csv")
+summary(sd)
+linear_model <- lm(formula = Salary ~ YearsExperience, data = sd)
+summary(linear_model)
+sd <- cbind(sd, linear_model$fitted.values)
+ggplot(data = sd, aes(x = YearsExperience, y = V2)) + 
+	geom_line() + 
+	geom_point(inherit.aes = FALSE, aes(x = YearsExperience, y = Salary), shape = "+", colour = "red", size = 2)
